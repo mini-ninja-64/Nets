c4d64570574cdb209b306dffda5ada98325b9218
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2017-03-19 22:49:37 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "equations.h"
+#include "Engine.h"
+
+int main(){
+  //init engine
+  //set start scene
+  while(true){
+    //Engine events
+    //Engine update
+    //Engine draw
+  }
+  return 0;
+}
